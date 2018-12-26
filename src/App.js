a7147db23fd37d2a5bbbce5ba250e2c299d6c877
Vitaliy Andrushko <vitalika1988@gmail.com>
2018-12-27 00:39:15 +0200

    initialize commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
