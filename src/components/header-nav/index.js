a87bcb81dff4d615414332239bf7a0cac49db24c
Vitaliy Andrushko <vitalika1988@gmail.com>
2019-01-20 13:47:51 +0200

    clean test

--- src/components/header-nav/index.js
+++ src/components/header-nav/index.js
@@ -40,11 +40,11 @@ export const NavItem = props => (
   <Flex
     width={37}
     height={35}
-    bg={H.ifElse(props.item.active, '#615d60', '#363135')}
     alignItems='center'
     justifyContent='center'
+    bg={H.ifElse(props.item.active, '#615d60', '#363135')}
   >
-    <IconWrapper opacity='0.9'>
+    <IconWrapper opacity={0.9}>
       <props.item.icon color={H.ifElse(props.item.active, '#e6dda6', '#ffffff')} />
     </IconWrapper>
   </Flex>
@@ -52,8 +52,8 @@ export const NavItem = props => (
 
 export const HeaderNav = props => (
   <Flex>
-    {getNavItems(props).map(item => (
-      <NavItem item={item} onClick={props.handleClickNavItem} />
+    {getNavItems(props).map((item, index) => (
+      <NavItem item={item} key={index} onClick={props.handleClickNavItem} />
     ))}
   </Flex>
 );
