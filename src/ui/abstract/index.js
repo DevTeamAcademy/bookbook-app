49943b63c731d4ef8083fa426aaed492b07ece4a
dmitriybodlev <dmitriybodlev@gmail.com>
2019-08-06 23:10:30 +0300

    BBA/Dima/1/feature/Deleted tests, added Abstract function

---
+++ src/ui/abstract/index.js
@@ -0,0 +1,95 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import {
+  top,
+  left,
+  flex,
+  size,
+  color,
+  space,
+  width,
+  right,
+  height,
+  bottom,
+  border,
+  opacity,
+  display,
+  position,
+  maxWidth,
+  minWidth,
+  fontSize,
+  textAlign,
+  maxHeight,
+  minHeight,
+  boxShadow,
+  borderTop,
+  fontWeight,
+  lineHeight,
+  borderLeft,
+  borderColor,
+  borderRight,
+  borderBottom,
+  borderRadius,
+} from 'styled-system';
+// //////////////////////////////////////////////////////////////////////////////
+
+export const Abstract = tag => styled(tag || 'div')`
+  ${top}
+  ${left}
+  ${flex}
+  ${size}
+  ${color}
+  ${space}
+  ${width}
+  ${right}
+  ${height}
+  ${bottom}
+  ${border}
+  ${opacity}
+  ${display}
+  ${fontSize}
+  ${position}
+  ${maxWidth}
+  ${minWidth}
+  ${textAlign}
+  ${maxHeight}
+  ${minHeight}
+  ${boxShadow}
+  ${borderTop}
+  ${fontWeight}
+  ${lineHeight}
+  ${borderLeft}
+  ${borderRight}
+  ${borderColor}
+  ${borderBottom}
+  ${borderRadius}
+  cursor: ${({ cursor }) => cursor}};
+`;
+
+export const AbstractPropTypes = {
+  ...top.propTypes,
+  ...left.propTypes,
+  ...size.propTypes,
+  ...color.propTypes,
+  ...space.propTypes,
+  ...width.propTypes,
+  ...right.propTypes,
+  ...height.propTypes,
+  ...bottom.propTypes,
+  ...border.propTypes,
+  ...opacity.propTypes,
+  ...display.propTypes,
+  ...position.propTypes,
+  ...maxWidth.propTypes,
+  ...minWidth.propTypes,
+  ...maxHeight.propTypes,
+  ...minHeight.propTypes,
+  ...boxShadow.propTypes,
+  ...borderTop.propTypes,
+  ...borderLeft.propTypes,
+  ...borderColor.propTypes,
+  ...borderRight.propTypes,
+  ...borderBottom.propTypes,
+  ...borderRadius.propTypes,
+  cursor: PropTypes.string,
+};
