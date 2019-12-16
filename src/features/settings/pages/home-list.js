deb52f419643f8821b16115a48df295ade8c2b15
dmitriybodlev <dmitriybodlev@gmail.com>
2019-12-16 16:35:46 +0200

    do settings page

---
+++ src/features/settings/pages/home-list.js
@@ -0,0 +1,105 @@
+// TODO: settings list here (with 2 options for now - languages and push notifications)
+import * as R from 'ramda';
+import PropTypes from 'prop-types';
+import React, { useContext, useState } from 'react';
+// components
+import Switch from '../../../components/form-fields/components/switch';
+// contexts
+import { LocaleContext } from '../../../contexts/locale';
+// helpers
+import * as H from '../../../helpers';
+//theme
+import Theme from '../../../theme';
+// ui
+import { Box, Flex, Text, PageWrapper } from '../../../ui';
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+
+const settingsSections = [
+  {
+    title: 'settings.homeList',
+    fieldSettings: [
+      {
+        name: 'quotes',
+        label: 'quotes',
+      },
+      {
+        name: 'quotes',
+        label: 'quotes',
+      },
+      {
+        name: 'quotes',
+        label: 'quotes',
+      },
+      {
+        name: 'quotes',
+        label: 'quotes',
+      },
+    ],
+  },
+];
+
+const fieldItem = item => {
+  H.getLocale(item.label);
+};
+
+const SettingsSection = props => {
+  return (
+    <Box
+      alignItems='center'
+      flexDirection='column'
+      justifyContent='space-between'
+      backgroundColor={Theme.colors.darkBlue}
+      borderBottom={Theme.borders.itemBorderBottom}
+    >
+      <Flex
+        p={15}
+        height={45}
+        fontWeight='bold'
+        alignItems='center'
+        color={Theme.colors.white}
+        justifyContent='space-between'
+        backgroundColor={Theme.colors.middleBlue}
+      >
+        {H.getLocale(props.item.title)}
+      </Flex>
+      {R.pathOr([], ['fieldSettings'], props.item).map((item, index) => (
+        <Flex
+          p={15}
+          key={index}
+          height={45}
+          fontWeight='bold'
+          alignItems='center'
+          justifyContent='space-between'
+          color={Theme.colors.inactiveGrey}
+          backgroundColor={Theme.colors.darkBlue}
+        >
+          <Box>{H.getLocale(item.label)}</Box>
+          <Switch
+            checked={R.path([item.label], props.values)}
+            handleChange={value => props.setValues(R.assoc(item.label, value, props.values))}
+          />
+        </Flex>
+      ))}
+    </Box>
+  );
+};
+
+export const HomeListSettingsPage = props => {
+  const [values, setValues] = useState({ notifications: false, quotes: false });
+  const { locale } = useContext(LocaleContext);
+  return (
+    <PageWrapper>
+      <Flex flexDirection='column'>
+        {settingsSections.map((item, index) => (
+          <SettingsSection key={index} item={item} values={values} setValues={setValues} />
+        ))}
+      </Flex>
+    </PageWrapper>
+  );
+};
+
+export default HomeListSettingsPage;
+
+HomeListSettingsPage.propTypes = {};
+
+HomeListSettingsPage.displayName = 'HomeListSettingsPage';
