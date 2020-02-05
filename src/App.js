5835e9ca7f4568a9bff8782c3711a9a673d342b4
Vitaliy Andrushko <vitalika1988@gmail.com>
2020-02-05 20:20:55 +0200

    Add-create-library-page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ErrorBoundary from 'react-error-boundary';
 import { ToastsContainer, ToastsStore } from 'react-toasts';
 // css
 import 'react-loading-bar/dist/index.css';
+// root
+import rootReducer from './reducer';
 // contexts
 import { LocaleProvider } from './contexts/locale';
 // global-state
@@ -14,10 +16,56 @@ import Routes from './routes';
 import GlobalStyles from './ui/global-styles';
 //  /////////////////////////////////////////////////////////////////////////////////////////////////
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo-hooks';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// import 'firebase/firestore' // <- needed if using firestore
+// import 'firebase/functions' // <- needed if using httpsCallable
+import { createStore, combineReducers, compose } from 'redux';
+import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase';
+// import { createFirestoreInstance, firestoreReducer } from 'redux-firestore'
 
-const client = new ApolloClient();
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from 'react-apollo-hooks';
+
+// const client = new ApolloClient();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_JNGWr-TlYsxQxGSILFuhfyyYfYqwmiA',
+  authDomain: 'bookbook-api-v1.firebaseapp.com',
+  databaseURL: 'https://bookbook-api-v1.firebaseio.com',
+  projectId: 'bookbook-api-v1',
+  storageBucket: 'bookbook-api-v1.appspot.com',
+  messagingSenderId: '271861363707',
+  appId: '1:271861363707:web:258548d4895aca4c26915e',
+  measurementId: 'G-NK7SJBYX65',
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+  // enableClaims: true // Get custom claims along with the profile
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Initialize other services on firebase instance
+// firebase.firestore() // <- needed if using firestore
+// firebase.functions() // <- needed if using httpsCallable
+
+// Create store with reducers and initial state
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  // createFirestoreInstance // <- needed if using firestore
+};
 
 const ErrorFallback = ({ error }) => {
   return (
@@ -31,15 +79,17 @@ const ErrorFallback = ({ error }) => {
 const App = () => (
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <ApolloProvider client={client}>
-        <GlobalStyles />
-        <ToastsContainer store={ToastsStore} />
-        <GlobalStateProvider>
-          <LocaleProvider>
-            <Routes />
-          </LocaleProvider>
-        </GlobalStateProvider>
-      </ApolloProvider>
+      <GlobalStyles />
+      <ToastsContainer store={ToastsStore} />
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <GlobalStateProvider>
+            <LocaleProvider>
+              <Routes />
+            </LocaleProvider>
+          </GlobalStateProvider>
+        </ReactReduxFirebaseProvider>
+      </Provider>
     </ErrorBoundary>
   </StrictMode>
 );
